b175ceebf87034b604388ec37231e6e9405f3dd9
William Ho <awho168@gmail.com>
2017-02-12 22:47:13 -0600

    Settings page set up. Added icons

--- views/layout.blade.php
+++ views/layout.blade.php
@@ -8,28 +8,42 @@
     <title>@yield('title')</title>
 
     <!--Fonts-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,400,500,800" rel="stylesheet" type="text/css">
 
     <!--Styles-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/navbar-fixed-left.css">
     <link rel="stylesheet" href="../css/app.css">
-    <link rel="stylesheet" href="../css/flipclock.css"
+    <link rel="stylesheet" href="../css/flipclock.css">
 
-    <!--Javascripts-->
+    <!--JavaScripts-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <script src="../js/Chart.js"></script>
     <script>var myChart = new Chart({...})</script>
+
+    <!--Website wide style definitions for fonts and colors-->
+    <style>
+        html, body {
+          background-color: #fff;
+          color: #636b6f;
+          font-family: 'Raleway', sans-serif;
+          font-weight: 100;
+          height: 100vh;
+          margin: 0;
+        }
+    </style>
 </head>
 <body>
   <div class="navbar navbar-inverse navbar-fixed-left">
     <a class="navbar-brand" href="/">Billy && Co.</a>
       <ul class="nav navbar-nav">
-        <li id="Dashboard Nav"><a href="/">Dashboard</a></li>
-        <li id="My Statistics Nav"><a href="myStats">My Statistics</a></li>
-        <li id="(Un)Productivity Nav"><a href="unproductivity">(Un)Productivity</a></li>
-        <li id="You vs. The World Nav"><a href="comparisons">You vs. The World</a></li>
-        <li id="Settings Nav"><a href="settings">Settings</a></li>
+        <li id="Dashboard Nav"><a href="/">Dashboard &nbsp;<i class="fa fa-tachometer" aria-hidden="true"></i></a></li>
+        <li id="My Statistics Nav"><a href="myStats">My Statistics &nbsp; <i class="fa fa-line-chart" aria-hidden="true"></i></a></li>
+        <li id="(Un)Productivity Nav"><a href="unproductivity">(Un)Productivity &nbsp; <i class="fa fa-hourglass-half" aria-hidden="true"></i></a></li>
+        <li id="You vs. The World Nav"><a href="comparisons">You vs. The World &nbsp; <i class="fa fa-globe" aria-hidden="true"></i></a></li>
+        <li id="Settings Nav"><a href="settings">Settings &nbsp; <i class="fa fa-cogs" aria-hidden="true"></i></a></li>
       </ul>
       <div id="clockdiv">
     <button id="inch"> &#9650; </button>
--- views/settings.blade.php
+++ views/settings.blade.php
@@ -5,4 +5,24 @@
 @section('content')
   <h1>Settings</h1>
   <hr/>
+  <h3>Categories</h2>
+    <div class="container">
+      Place content
+    </div>
+  <h3>Default Timer</h3>
+    <div class="container">
+      Place content
+    </div>
+  <h3>My Statistics Page</h3>
+    <div class="container">
+      Place content
+    </div>
+  <h3>(Un)Productivity Page</h3>
+    <div class="container">
+      Place content
+    </div>
+  <h3>You vs. The World Page</h3>
+    <div class="container">
+      Place content
+    </div>
 @endsection
