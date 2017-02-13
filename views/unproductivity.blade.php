7a1777b2ac32950b17987ecfea3dcbce4a93d21f
sarahjwong <sarahwong2019@u.northwestern.edu>
2017-02-13 12:15:36 -0600

    Edited Unproductivity

--- views/unproductivity.blade.php
+++ views/unproductivity.blade.php
@@ -5,4 +5,19 @@
 @section('content')
   <h1>(Un)Productivity</h1>
   <hr/>
+<h2>Top 3 Websites</h2> 
+<h2>You Spent:</h2>
+<br>
+    <div class="container">
+
+ 
+    <ul class = "unprodBody">
+        <li> 3 hours on Facebook. You could have completed two problem sets.</li>
+        <br>
+        <li>4 hours on Canvas. You could have ordered Chinese, had it delivered, and finished two episodes on Netflix.</li>
+        <br>
+        <li>1.5 hours on Netflix. You could have grabbed dinner with a friend.</li>
+    </ul>
+</div>
+
 @endsection
