7d405f8d33cede7d5f3d3941b5ec81a6942efed4
rlee303 <richielee826@yahoo.com>
2017-02-18 17:29:57 -0600

    by force

--- views/unproductivity.blade.php
+++ views/unproductivity.blade.php
@@ -9,11 +9,11 @@
 <h2>You Spent:</h2>
 <br>
     <div id="unprodText" class="container-embed">
-        <p class = 'site1'> 3 hours on Facebook. You could have completed two problem sets.</p>
+        <p class = 'site1'> 3 hours on <span id="fb">Facebook</span>. You could have completed two problem sets.</p>
         <br>
-        <p class = 'site2'>4 hours on Canvas. You could have ordered Chinese food, had it delivered, and finished two episodes on Netflix.</p>
+        <p class = 'site2'>4 hours on <span id="cs">Caesar</span>. You could have ordered Chinese food, had it delivered, and finished two episodes on Netflix.</p>
         <br>
-        <p class = 'site3'>1.5 hours on Netflix. You could have grabbed dinner with a friend.</p>
+        <p class = 'site3'>1.5 hours on <span id="nf">Netflix</span>. You could have grabbed dinner with a friend.</p>
 </div>
 
 @endsection
