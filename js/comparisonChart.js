a4ad0b52431908c4904251f606aa7b5b0aaa3404
Peter Bi <peter@dhcp-10-105-95-220.wireless.northwestern.private>
2017-02-18 17:39:40 -0600

    added feature of button changing data on graph based on categories

--- js/comparisonChart.js
+++ js/comparisonChart.js
@@ -1,24 +1,117 @@
 var data = {
     datasets: [{
-      data: [60, 35, 24, 40, 30, 22],
+      data: [35, 24, 40, 30, 22],
       label:"You",
       backgroundColor:"rgba(255, 99, 132, 0.2)"
      },
      {
-      data: [40, 45, 40, 25, 25, 50],
+      data: [45, 40, 25, 25, 50],
       label:"The World",
       backgroundColor:"rgba(255, 0, 132, 0.2)"
      }],
-  labels: ["Facebook", 'YouTube', 'Piazza', 'Canvas','Caesar', 'Gmail']
+  labels: ['YouTube', 'Piazza', 'Canvas','Caesar', 'Gmail']
+};
 
+//onclick event for the menu of categories
+document.getElementById('socialMedia').onclick = function() {
+barChart.data.datasets[0].data[0] = 60;
+barChart.data.datasets[0].data[1] = 35;
+barChart.data.datasets[0].data[2] = 40;
+barChart.data.datasets[0].data[3] = 22;
+barChart.data.datasets[0].data[4] = 37;
+barChart.data.datasets[1].data[0] = 40;
+barChart.data.datasets[1].data[1] = 45;
+barChart.data.datasets[1].data[2] = 50;
+barChart.data.datasets[1].data[3] = 50;
+barChart.data.datasets[1].data[4] = 30;
+barChart.data.labels[0] = "Facebook";
+barChart.data.labels[1] = "Instagram";
+barChart.data.labels[2] = "Twitter";
+barChart.data.labels[3] = "GroupMe";
+barChart.data.labels[4] = "Douban";
+barChart.update();
 };
-var options = {
+
+document.getElementById('school').onclick = function() {
+barChart.data.datasets[0].data[0] = 30;
+barChart.data.datasets[0].data[1] = 35;
+barChart.data.datasets[0].data[2] = 40;
+barChart.data.datasets[0].data[3] = 32;
+barChart.data.datasets[0].data[4] = 34;
+barChart.data.datasets[1].data[0] = 59;
+barChart.data.datasets[1].data[1] = 40;
+barChart.data.datasets[1].data[2] = 50;
+barChart.data.datasets[1].data[3] = 55;
+barChart.data.datasets[1].data[4] = 25;
+barChart.data.labels[0] = "CourseHero";
+barChart.data.labels[1] = "Chegg";
+barChart.data.labels[2] = "Piazza";
+barChart.data.labels[3] = "Canvas";
+barChart.data.labels[4] = "Caesar";
+barChart.update();
 };
+
+document.getElementById('entertainment').onclick = function() {
+barChart.data.datasets[0].data[0] = 50;
+barChart.data.datasets[0].data[1] = 70;
+barChart.data.datasets[0].data[2] = 40;
+barChart.data.datasets[0].data[3] = 25;
+barChart.data.datasets[0].data[4] = 25;
+barChart.data.datasets[1].data[0] = 60;
+barChart.data.datasets[1].data[1] = 50;
+barChart.data.datasets[1].data[2] = 30;
+barChart.data.datasets[1].data[3] = 30;
+barChart.data.datasets[1].data[4] = 25;
+barChart.data.labels[0] = "YouTube";
+barChart.data.labels[1] = "Netflix";
+barChart.data.labels[2] = "HBO";
+barChart.data.labels[3] = "Vine";
+barChart.data.labels[4] = "Vimeo";
+barChart.update();
+};
+
+document.getElementById('news').onclick = function() {
+barChart.data.datasets[0].data[0] = 30;
+barChart.data.datasets[0].data[1] = 30;
+barChart.data.datasets[0].data[2] = 24;
+barChart.data.datasets[0].data[3] = 22;
+barChart.data.datasets[0].data[4] = 37;
+barChart.data.datasets[1].data[0] = 40;
+barChart.data.datasets[1].data[1] = 15;
+barChart.data.datasets[1].data[2] = 45;
+barChart.data.datasets[1].data[3] = 50;
+barChart.data.datasets[1].data[4] = 35;
+barChart.data.labels[0] = "CNN";
+barChart.data.labels[1] = "Daily Northwestern";
+barChart.data.labels[2] = "Washington Post";
+barChart.data.labels[3] = "Guardian";
+barChart.data.labels[4] = "Axios";
+barChart.update();
+};
+/*
+document.getElementById('email').onclick = function() {
+barChart.data.datasets[0].data[0] = 60;
+barChart.data.datasets[0].data[1] = 35;
+barChart.data.datasets[0].data[2] = 24;
+barChart.data.datasets[0].data[3] = 22;
+barChart.data.datasets[0].data[4] = 37;
+barChart.data.datasets[1].data[0] = 40;
+barChart.data.datasets[1].data[1] = 45;
+barChart.data.datasets[1].data[2] = 25;
+barChart.data.datasets[1].data[3] = 50;
+barChart.data.datasets[1].data[4] = 15;
+barChart.data.labels[0] = "Facebook";
+barChart.data.labels[1] = "Instagram";
+barChart.data.labels[2] = "Twitter";
+barChart.data.labels[3] = "GroupMe";
+barChart.data.labels[4] = "Douban";
+barChart.update();
+};
+*/
 var ctx3= document.getElementById("comparisonChart").getContext("2d");
 var barChart = new Chart(ctx3, {
     type: 'bar',
     data: data,
-    options: options
 });
 
 function adjustChartSize(){
