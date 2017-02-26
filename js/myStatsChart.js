5ac721e0023284072cd4d43e68d0c4da4fac263e
Academic Consultant <aco580@publicmedia07.library.northwestern.edu>
2017-02-26 17:55:44 -0600

    unfinsihed

--- js/myStatsChart.js
+++ js/myStatsChart.js
@@ -24,7 +24,7 @@ function setAllButtonDefault() {
 //default data, does not matter what it is, just place holder, do not delete
 var defaultData = {
     datasets: [{
-      data: [60, 35, 24, 40, 30],
+      data: [0, 0, 0, 0, 0],
       backgroundColor: [
             "rgba(59, 89, 152, 1)",  //Facebook
             "rgba(187, 0, 0, 1)",   //YouTube
@@ -86,167 +86,178 @@ var myPolarChart = new Chart(ctx2, {
 
 });
 
-updateData();
+
+function initBar() {
+	if (category == "Social Media") {
+	myBarChart.data.datasets[0].data[0] = 60;
+	myBarChart.data.datasets[0].data[1] = 35;
+	myBarChart.data.datasets[0].data[2] = 40;
+	myBarChart.data.datasets[0].data[3] = 22;
+	myBarChart.data.datasets[0].data[4] = 37;
+	myBarChart.data.labels[0] = "Facebook";
+	myBarChart.data.labels[1] = "Instagram";
+	myBarChart.data.labels[2] = "Twitter";
+	myBarChart.data.labels[3] = "GroupMe";
+	myBarChart.data.labels[4] = "Douban";
+	myBarChart.update();
+}
+}
+
+initBar();
+//updateData();
 //////////////////////////////////////////////////////////////
 
 //onclick event for the menu of categories
 document.getElementById('socialMedia').onclick = function() {
 setAllButtonDefault();
 category = 'Social Media';
-localStorage.setItem("category", 'Social Media');
-updateData();
+//localStorage.setItem("category", 'Social Media');
+myBarChart.data.datasets[0].data[0] = 60;
+myBarChart.data.datasets[0].data[1] = 35;
+myBarChart.data.datasets[0].data[2] = 40;
+myBarChart.data.datasets[0].data[3] = 22;
+myBarChart.data.datasets[0].data[4] = 37;
+myBarChart.data.labels[0] = "Facebook";
+myBarChart.data.labels[1] = "Instagram";
+myBarChart.data.labels[2] = "Twitter";
+myBarChart.data.labels[3] = "GroupMe";
+myBarChart.data.labels[4] = "Douban";
+//insert background color like this for bar chart:  backgroundColor[i] where i is the index of data
+//			myBarChart.data.datasets[0].backgroundColor[0] = "rgba(78, 45, 131, 1)";
+
+myPolarChart.data.datasets[0].data[0] = 60;
+myPolarChart.data.datasets[0].data[1] = 35;
+myPolarChart.data.datasets[0].data[2] = 40;
+myPolarChart.data.datasets[0].data[3] = 22;
+myPolarChart.data.datasets[0].data[4] = 37;
+myPolarChart.data.labels[0] = "Facebook";
+myPolarChart.data.labels[1] = "Instagram";
+myPolarChart.data.labels[2] = "Twitter";
+myPolarChart.data.labels[3] = "GroupMe";
+myPolarChart.data.labels[4] = "Douban";
+//insert background color like this for polar chart:  backgroundColor[i] where i is the index of data
+//			myPolarChart.data.datasets[0].backgroundColor[0] = "rgba(78, 45, 131, 1)";
+
+document.getElementById('socialMedia').className = "btn button btn-active";
+myPolarChart.update();
+myBarChart.update();
 };
 
 document.getElementById('school').onclick = function() {
 setAllButtonDefault();
 category = 'School';
-localStorage.setItem("category", 'School');
-updateData();
+//localStorage.setItem("category", 'School');
+myBarChart.data.datasets[0].data[0] = 30;
+myBarChart.data.datasets[0].data[1] = 35;
+myBarChart.data.datasets[0].data[2] = 40;
+myBarChart.data.datasets[0].data[3] = 32;
+myBarChart.data.datasets[0].data[4] = 34;
+myBarChart.data.labels[0] = "Scribd";
+myBarChart.data.labels[1] = "Chegg";
+myBarChart.data.labels[2] = "Piazza";
+myBarChart.data.labels[3] = "Canvas";
+myBarChart.data.labels[4] = "Caesar";
+myPolarChart.data.datasets[0].data[0] = 30;
+myPolarChart.data.datasets[0].data[1] = 35;
+myPolarChart.data.datasets[0].data[2] = 40;
+myPolarChart.data.datasets[0].data[3] = 32;
+myPolarChart.data.datasets[0].data[4] = 34;
+myPolarChart.data.labels[0] = "Scribd";
+myPolarChart.data.labels[1] = "Chegg";
+myPolarChart.data.labels[2] = "Piazza";
+myPolarChart.data.labels[3] = "Canvas";
+myPolarChart.data.labels[4] = "Caesar";
+ document.getElementById('school').className = "btn button btn-active";
+ myPolarChart.update();
+ myBarChart.update();
 };
 
 document.getElementById('entertainment').onclick = function() {
 setAllButtonDefault();
 category = 'Entertainment';
-localStorage.setItem("category", 'Entertainment');
-updateData();
-};
+//localStorage.setItem("category", 'Entertainment');
+myBarChart.data.datasets[0].data[0] = 50;
+myBarChart.data.datasets[0].data[1] = 70;
+myBarChart.data.datasets[0].data[2] = 40;
+myBarChart.data.datasets[0].data[3] = 25;
+myBarChart.data.datasets[0].data[4] = 25;
+myBarChart.data.labels[0] = "YouTube";
+myBarChart.data.labels[1] = "Netflix";
+myBarChart.data.labels[2] = "HBO";
+myBarChart.data.labels[3] = "Vine";
+myBarChart.data.labels[4] = "Vimeo";
+myPolarChart.data.datasets[0].data[0] = 50;
+myPolarChart.data.datasets[0].data[1] = 70;
+myPolarChart.data.datasets[0].data[2] = 40;
+myPolarChart.data.datasets[0].data[3] = 25;
+myPolarChart.data.datasets[0].data[4] = 25;
+myPolarChart.data.labels[0] = "YouTube";
+myPolarChart.data.labels[1] = "Netflix";
+myPolarChart.data.labels[2] = "HBO";
+myPolarChart.data.labels[3] = "Vine";
+myPolarChart.data.labels[4] = "Vimeo";
+	 document.getElementById('entertainment').className = "btn button btn-active";
+	 myPolarChart.update();
+	 myBarChart.update();
+ };
 
 document.getElementById('news').onclick = function() {
 setAllButtonDefault();
 category = 'News';
-localStorage.setItem("category", 'News');
-updateData();
+//localStorage.setItem("category", 'News');
+myBarChart.data.datasets[0].data[0] = 30;
+myBarChart.data.datasets[0].data[1] = 30;
+myBarChart.data.datasets[0].data[2] = 24;
+myBarChart.data.datasets[0].data[3] = 22;
+myBarChart.data.datasets[0].data[4] = 37;
+myBarChart.data.labels[0] = "CNN";
+myBarChart.data.labels[1] = "Daily";
+myBarChart.data.labels[2] = "NY Times";
+myBarChart.data.labels[3] = "Guardian";
+myBarChart.data.labels[4] = "Axios";
+myPolarChart.data.datasets[0].data[0] = 30;
+myPolarChart.data.datasets[0].data[1] = 30;
+myPolarChart.data.datasets[0].data[2] = 24;
+myPolarChart.data.datasets[0].data[3] = 22;
+myPolarChart.data.datasets[0].data[4] = 37;
+myPolarChart.data.labels[0] = "CNN";
+myPolarChart.data.labels[1] = "Daily";
+myPolarChart.data.labels[2] = "NY Times";
+myPolarChart.data.labels[3] = "Guardian";
+myPolarChart.data.labels[4] = "Axios";
+document.getElementById('news').className = "btn button btn-active";
+myPolarChart.update();
+myBarChart.update();
 };
 
 document.getElementById('shopping').onclick = function() {
 setAllButtonDefault();
 category = 'Shopping';
-localStorage.setItem("category", 'Shopping');
-updateData();
+//localStorage.setItem("category", 'Shopping');
+myBarChart.data.datasets[0].data[0] = 60;
+myBarChart.data.datasets[0].data[1] = 35;
+myBarChart.data.datasets[0].data[2] = 24;
+myBarChart.data.datasets[0].data[3] = 22;
+myBarChart.data.datasets[0].data[4] = 37;
+myBarChart.data.labels[0] = "Amazon";
+myBarChart.data.labels[1] = "EBay";
+myBarChart.data.labels[2] = "CraigsList";
+myBarChart.data.labels[3] = "Alibaba";
+myBarChart.data.labels[4] = "Taobao";
+myPolarChart.data.datasets[0].data[0] = 60;
+myPolarChart.data.datasets[0].data[1] = 35;
+myPolarChart.data.datasets[0].data[2] = 24;
+myPolarChart.data.datasets[0].data[3] = 22;
+myPolarChart.data.datasets[0].data[4] = 37;
+myPolarChart.data.labels[0] = "Amazon";
+myPolarChart.data.labels[1] = "EBay";
+myPolarChart.data.labels[2] = "CraigsList";
+myPolarChart.data.labels[3] = "Alibaba";
+myPolarChart.data.labels[4] = "Taobao";
+ document.getElementById('shopping').className = "btn button btn-active";
+ myPolarChart.update();
+ myBarChart.update();
 };
 
-function updateData() {
-		setAllButtonDefault();
-		/*  select dataset based on category clicked */
-		if (category == "Social Media") {
-			myBarChart.data.datasets[0].data[0] = 60;
-			myBarChart.data.datasets[0].data[1] = 35;
-			myBarChart.data.datasets[0].data[2] = 40;
-			myBarChart.data.datasets[0].data[3] = 22;
-			myBarChart.data.datasets[0].data[4] = 37;
-			myBarChart.data.labels[0] = "Facebook";
-			myBarChart.data.labels[1] = "Instagram";
-			myBarChart.data.labels[2] = "Twitter";
-			myBarChart.data.labels[3] = "GroupMe";
-			myBarChart.data.labels[4] = "Douban";
-			//insert background color like this for bar chart:  backgroundColor[i] where i is the index of data
-//			myBarChart.data.datasets[0].backgroundColor[0] = "rgba(78, 45, 131, 1)";
-
-			myPolarChart.data.datasets[0].data[0] = 60;
-			myPolarChart.data.datasets[0].data[1] = 35;
-			myPolarChart.data.datasets[0].data[2] = 40;
-			myPolarChart.data.datasets[0].data[3] = 22;
-			myPolarChart.data.datasets[0].data[4] = 37;
-			myPolarChart.data.labels[0] = "Facebook";
-			myPolarChart.data.labels[1] = "Instagram";
-			myPolarChart.data.labels[2] = "Twitter";
-			myPolarChart.data.labels[3] = "GroupMe";
-			myPolarChart.data.labels[4] = "Douban";
-			//insert background color like this for polar chart:  backgroundColor[i] where i is the index of data
-//			myPolarChart.data.datasets[0].backgroundColor[0] = "rgba(78, 45, 131, 1)";
-
-			document.getElementById('socialMedia').className = "btn button btn-active";
-		}else if (category == "School") {
-			myBarChart.data.datasets[0].data[0] = 30;
-			myBarChart.data.datasets[0].data[1] = 35;
-			myBarChart.data.datasets[0].data[2] = 40;
-			myBarChart.data.datasets[0].data[3] = 32;
-			myBarChart.data.datasets[0].data[4] = 34;
-			myBarChart.data.labels[0] = "Scribd";
-			myBarChart.data.labels[1] = "Chegg";
-			myBarChart.data.labels[2] = "Piazza";
-			myBarChart.data.labels[3] = "Canvas";
-			myBarChart.data.labels[4] = "Caesar";
-			myPolarChart.data.datasets[0].data[0] = 30;
-			myPolarChart.data.datasets[0].data[1] = 35;
-			myPolarChart.data.datasets[0].data[2] = 40;
-			myPolarChart.data.datasets[0].data[3] = 32;
-			myPolarChart.data.datasets[0].data[4] = 34;
-			myPolarChart.data.labels[0] = "Scribd";
-			myPolarChart.data.labels[1] = "Chegg";
-			myPolarChart.data.labels[2] = "Piazza";
-			myPolarChart.data.labels[3] = "Canvas";
-			myPolarChart.data.labels[4] = "Caesar";
-		   document.getElementById('school').className = "btn button btn-active";
-		}else if (category == "News") {
-			myBarChart.data.datasets[0].data[0] = 30;
-			myBarChart.data.datasets[0].data[1] = 30;
-			myBarChart.data.datasets[0].data[2] = 24;
-			myBarChart.data.datasets[0].data[3] = 22;
-			myBarChart.data.datasets[0].data[4] = 37;
-			myBarChart.data.labels[0] = "CNN";
-			myBarChart.data.labels[1] = "Daily";
-			myBarChart.data.labels[2] = "NY Times";
-			myBarChart.data.labels[3] = "Guardian";
-			myBarChart.data.labels[4] = "Axios";
-			myPolarChart.data.datasets[0].data[0] = 30;
-			myPolarChart.data.datasets[0].data[1] = 30;
-			myPolarChart.data.datasets[0].data[2] = 24;
-			myPolarChart.data.datasets[0].data[3] = 22;
-			myPolarChart.data.datasets[0].data[4] = 37;
-			myPolarChart.data.labels[0] = "CNN";
-			myPolarChart.data.labels[1] = "Daily";
-			myPolarChart.data.labels[2] = "NY Times";
-			myPolarChart.data.labels[3] = "Guardian";
-			myPolarChart.data.labels[4] = "Axios";
-		 document.getElementById('news').className = "btn button btn-active";
-	 }else if (category == "Entertainment") {
-			myBarChart.data.datasets[0].data[0] = 50;
-			myBarChart.data.datasets[0].data[1] = 70;
-			myBarChart.data.datasets[0].data[2] = 40;
-			myBarChart.data.datasets[0].data[3] = 25;
-			myBarChart.data.datasets[0].data[4] = 25;
-			myBarChart.data.labels[0] = "YouTube";
-			myBarChart.data.labels[1] = "Netflix";
-			myBarChart.data.labels[2] = "HBO";
-			myBarChart.data.labels[3] = "Vine";
-			myBarChart.data.labels[4] = "Vimeo";
-			myPolarChart.data.datasets[0].data[0] = 50;
-		  myPolarChart.data.datasets[0].data[1] = 70;
-		  myPolarChart.data.datasets[0].data[2] = 40;
-		  myPolarChart.data.datasets[0].data[3] = 25;
-		  myPolarChart.data.datasets[0].data[4] = 25;
-		  myPolarChart.data.labels[0] = "YouTube";
-		  myPolarChart.data.labels[1] = "Netflix";
-		  myPolarChart.data.labels[2] = "HBO";
-		  myPolarChart.data.labels[3] = "Vine";
-		  myPolarChart.data.labels[4] = "Vimeo";
-			   document.getElementById('entertainment').className = "btn button btn-active";
-		}else if (category == "Shopping") {
-			myBarChart.data.datasets[0].data[0] = 60;
-			myBarChart.data.datasets[0].data[1] = 35;
-			myBarChart.data.datasets[0].data[2] = 24;
-			myBarChart.data.datasets[0].data[3] = 22;
-			myBarChart.data.datasets[0].data[4] = 37;
-			myBarChart.data.labels[0] = "Amazon";
-			myBarChart.data.labels[1] = "EBay";
-			myBarChart.data.labels[2] = "CraigsList";
-			myBarChart.data.labels[3] = "Alibaba";
-			myBarChart.data.labels[4] = "Taobao";
-			myPolarChart.data.datasets[0].data[0] = 60;
-		  myPolarChart.data.datasets[0].data[1] = 35;
-		  myPolarChart.data.datasets[0].data[2] = 24;
-		  myPolarChart.data.datasets[0].data[3] = 22;
-		  myPolarChart.data.datasets[0].data[4] = 37;
-		  myPolarChart.data.labels[0] = "Amazon";
-		  myPolarChart.data.labels[1] = "EBay";
-		  myPolarChart.data.labels[2] = "CraigsList";
-		  myPolarChart.data.labels[3] = "Alibaba";
-		  myPolarChart.data.labels[4] = "Taobao";
-			 document.getElementById('shopping').className = "btn button btn-active";
-		}
-		myPolarChart.update();
-		myBarChart.update();
-}
 
 ///////////////////////////////////////////////////////////////////
