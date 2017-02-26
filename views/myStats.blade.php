ce7542cce4ab785e79c51c761ebcea238f160b9f
William Cang-Yi Ho <wch002@publicmedia01.library.northwestern.edu>
2017-02-26 17:34:38 -0600

    Modified mystats

--- views/myStats.blade.php
+++ views/myStats.blade.php
@@ -5,15 +5,13 @@
 @section('content')
   <h1>My Statistics</h1>
   <hr/>
-
-<div class="btn-group">
-      <button id="socialMedia" class="btn button btn-active">Social Media</button>
-      <button id="school" class="btn button">School</button>
-      <button id="entertainment" class="btn button">Entertainment</button>
-      <button id="news" class="btn button">News</button>
-      <button id="shopping" class="btn button">Shopping</button> 
-    </div>
-
+  <div class="btn-group">
+    <button id="socialMedia" class="btn button btn-active">Social Media</button>
+    <button id="school" class="btn button">School</button>
+    <button id="entertainment" class="btn button">Entertainment</button>
+    <button id="news" class="btn button">News</button>
+    <button id="shopping" class="btn button">Shopping</button>
+  </div>
   <div id="myStats_canvas_bar" class="row">
     <canvas id="barChart" width="40%" height="40%"></canvas>
     <script src="../js/myStatsChart.js"></script>
